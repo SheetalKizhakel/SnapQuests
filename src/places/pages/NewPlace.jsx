ca7a2f632e5b7a728c4ecf39719b6bb38a434167
SheetalKizhakel <kizhakelsheetal@gmail.com>
2024-06-23 13:59:55 +0530

    maps implemented

--- src/places/pages/NewPlace.jsx
+++
@@ -1,9 +0,0 @@
-//page when you want to create a new place
-import react from "react";
-function NewPlace()
-{
-    return(
-        <h2>NewPLace works!</h2>
-    )
-}
-export default NewPlace;
--- src/shared_components/UIElements/Map.jsx
+++ src/shared_components/UIElements/Map.jsx
@@ -1,15 +1,15 @@
 import React, { useRef, useEffect } from 'react';
- 
 import './Map.css';
- 
+
+
 const Map = props => {
   const mapRef = useRef();
-  
+
   const { center, zoom } = props;
- 
+
   useEffect(() => {
-    new window.ol.Map({
-      target: mapRef.current.id,
+    const map = new window.ol.Map({
+      target: mapRef.current,
       layers: [
         new window.ol.layer.Tile({
           source: new window.ol.source.OSM()
@@ -20,8 +20,34 @@ const Map = props => {
         zoom: zoom
       })
     });
+
+    // Add a marker
+    const marker = new window.ol.Feature({
+      geometry: new window.ol.geom.Point(window.ol.proj.fromLonLat([center.lng, center.lat])),
+    });
+
+    const markerStyle = new window.ol.style.Style({
+      image: new window.ol.style.Icon({
+        anchor: [0.5, 1],
+        src: 'https://images.ctfassets.net/3prze68gbwl1/assetglossary-17su9wok1ui0z7w/c4c4bdcdf0d0f86447d3efc450d1d081/map-marker.png',
+        height:45
+      })
+    });
+
+    marker.setStyle(markerStyle);
+
+    const vectorSource = new window.ol.source.Vector({
+      features: [marker]
+    });
+
+    const vectorLayer = new window.ol.layer.Vector({
+      source: vectorSource
+    });
+
+    map.addLayer(vectorLayer);
+
   }, [center, zoom]);
- 
+
   return (
     <div
       ref={mapRef}
@@ -31,5 +57,5 @@ const Map = props => {
     ></div>
   );
 };
- 
+
 export default Map;
