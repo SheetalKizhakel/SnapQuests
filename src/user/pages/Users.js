257617bcb7da5a468c41600169038b1e0fa0539e
SheetalKizhakel <kizhakelsheetal@gmail.com>
2024-07-06 08:29:37 +0530

    frontend dockerfile added

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,7 +4,7 @@ import UsersList from '../components/UsersList';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { useHttpClient } from '../../shared/components/hooks/http-hook';
-import Three from '../pages/Three';
+import Three from './Three';
 
 const Users = () => {
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
